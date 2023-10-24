a0ec01b90d058167d9843748da11f19d78f64037
lasha <60874161+lashatarkhnishvili2001@users.noreply.github.com>
2023-10-24 16:30:45 +0400

    first

--- src/Components/DetailsPage/Rectangle-detail/RectangleDetail.jsx
+++ src/Components/DetailsPage/Rectangle-detail/RectangleDetail.jsx
@@ -1,78 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Heading6, Subheading } from '../../Headings'
 import image from '../../../Assets/images/clothes/image 30.png';
 import './RectangleDetail.css'
+import { getLatestProducts } from '../../../store/lateslProducts/lateslProducts';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 const RectangleDetail = () => {
+
+    const navigate = useNavigate()
+
+    const dispatch = useDispatch()
+
+    const {latestProducts } = useSelector((state) => state.latestProducts);
+
+
+    useEffect(() => {
+        dispatch(getLatestProducts() )
+    },[dispatch]) 
+
     return (
         <div className='RectangleDetail'>
             <Heading6 text={'You may like'} />
             <div className="RectangleColumn">
                 <ul>
-                    <li>
-                        <div className="listRow">
-                            <div className="image">
-                            <img src={image} alt="" />
-                            </div>
-                            <div className="destination">
-                                <div className='black'>
-                                    <Subheading  text={'Apple Watch Series Space Gray'}/>
+                    {latestProducts.map((item) => {
+                        return (
+                        <li onClick={() => {navigate(`/details/${item.id}`)}}>
+                            <div className="listRow" >
+                                <div className="image">
+                                <img src={item.images[0]} alt="" />
                                 </div>
-                                <Subheading text={'$7.00 - $99.50'}/>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listRow">
-                            <div className="image">
-                            <img src={image} alt="" />
-                            </div>
-                            <div className="destination">
-                                <div className='black'>
-                                    <Subheading  text={'Apple Watch Series Space Gray'}/>
+                                <div className="destination">
+                                    <div className='black'>
+                                        <Subheading  text={item.brand}/>
+                                    </div>
+                                    <Subheading text={item.price}/>
                                 </div>
-                                <Subheading text={'$7.00 - $99.50'}/>
                             </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listRow">
-                            <div className="image">
-                            <img src={image} alt="" />
-                            </div>
-                            <div className="destination">
-                                <div className='black'>
-                                    <Subheading  text={'Apple Watch Series Space Gray'}/>
-                                </div>
-                                <Subheading text={'$7.00 - $99.50'}/>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listRow">
-                            <div className="image">
-                            <img src={image} alt="" />
-                            </div>
-                            <div className="destination">
-                                <div className='black'>
-                                    <Subheading  text={'Apple Watch Series Space Gray'}/>
-                                </div>
-                                <Subheading text={'$7.00 - $99.50'}/>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="listRow">
-                            <div className="image">
-                            <img src={image} alt="" />
-                            </div>
-                            <div className="destination">
-                                <div className='black'>
-                                    <Subheading  text={'Apple Watch Series Space Gray'}/>
-                                </div>
-                                <Subheading text={'$7.00 - $99.50'}/>
-                            </div>
-                        </div>
-                    </li>
+                        </li>
+                        )
+                    })}
+                    
                     
                 </ul>
             </div>
